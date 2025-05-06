15c60a5e550fe906c177139addcd3bad138e253a
Sihui Zhong <Sihui.Zhong@warwick.ac.uk>
2025-05-06 10:37:48 +0100

    Update nonlinear_model.py

--- nonlinear_model.py
+++ nonlinear_model.py
@@ -26,13 +26,13 @@ def nonlinear_model(t, R, roi, period_k, period_A, Vi, delta_m, zeta, C1=0.3):
     wS = (wA+wk)/2.0
     dV = Vi*1.1
     H = pi*R/4 #depth
-    E = 2*R*H*roi #mass in the core
+    E = 4*R*H*roi #mass in the core
     A = E*Vi
     M_bar = (Vi - dV*np.sqrt(roe)/(np.sqrt(roi)+np.sqrt(roe)))*zeta
     #B--momentum loss rate of the tube core
     B = 1/(2*H)*C2/np.sqrt(2)*np.sqrt(roe)/(np.sqrt(roi)+np.sqrt(roe))*(roi*roe)**(0.25)/(np.sqrt(roi)+np.sqrt(roe))*dV
     #print('B=',B,'1/B (time at zero M_core)=',1/B)
-    D = M_bar*C2/np.sqrt(2)*(roi*roe)**(0.25)/(np.sqrt(roi)+np.sqrt(roe))*dV*R/wD
+    D = 2*M_bar*C2/np.sqrt(2)*(roi*roe)**(0.25)/(np.sqrt(roi)+np.sqrt(roe))*dV*R/wD
     #G --mass gain/loss rate in the area defined by rho_T-->delta_m
     G = delta_m/(2*H*roi)*C2/np.sqrt(2)*(roi*roe)**(0.25)/(np.sqrt(roi)+np.sqrt(roe))*dV
     A1 = A/(E*(G**2))*(-B*G*np.sin(wk*t)/wk+(B+G)*(np.cos(wk/G)*sici(wk*(t+1/G))[1]+np.sin(wk/G)*sici(wk*(t+1/G))[0]))
